64dc5f74d36f37ca6f3499cc2f497d54c7270177
Emma su <suyutong1997@gmail.com>
2023-09-28 14:56:34 -0400

    header-component-for-review

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
+
 
 function App() {
 
-  return <>HELLO</>;
+  return (
+  
+  
+  <Header />
+  
+  
+    
+    )
+  
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,33 @@
+import "./Header.scss";
+
+
+import logo from "./InStock-Logo_1x.png";
+
+
 export default function Header() {
-    return <></>
+    return(
+        <div className="header__allcontainer">
+            <div className="header__container">
+                <div className="header__logocontainer">
+                    <img src={logo}></img>
+                </div>
+
+                <div className="header__twobuttonscontainer">
+                    <div className="header__buttoncontainer">
+                        <button type='button' className="header__buttoncontainer-warehouses" >Warehouses</button>
+                    </div>
+                    
+                    <div className="header__buttoncontainer">
+                        <button type='button' className="header__buttoncontainer-invebtory" >Inventory</button>
+                    </div>
+                
+
+                </div>
+            </div>
+    
+        </div>
+
+
+    ) 
+    
 }
