5d4c9ac314de0c8fa44bc388028d09c6ef7beb22
Patrick Harvey <patharvey@protonmail.com>
2023-09-28 12:52:07 -0400

    set up structure of add-edit inventory item

--- src/components/EditInventory/EditInventory.jsx
+++ src/components/EditInventory/EditInventory.jsx
@@ -1,19 +1,43 @@
 export default function EditInventory() {
-    return (
-      <>
-        <div className="title">
-          <h1 className="title__text">Edit Inventory Item</h1>
-        </div>
+  return (
+    <>
+      <div className="title">
+        <h1 className="title__text">Edit Inventory Item</h1>
+      </div>
+      <form className="form">
         <div className="details">
           <h2 className="details__title">Item Details</h2>
-              <form>
-                  <label>Item Name</label>
-                  <input></input>
-                  <label>Description</label>
-                  <input></input>
-                  <label>Category</label>
-              </form>
+          <label>Item Name</label>
+          <input
+            placeholder="Item Name"
+            name="item-name"
+            value={itemName}
+            onChange={handleItemName}
+          ></input>
+          <label>Description</label>
+          <textarea
+            placeholder="Please enter a brief item description..."
+            name="item-description"
+            value={itemDesc}
+            onChange={handleItemDesc}
+          ></textarea>
+          <label>Category</label>
         </div>
-      </>
-    );
-  }
+        <div className="availablity">
+          <h2 className="availability__title">Item Availability</h2>
+          <h3>Status</h3>
+            <input id="inStock" name="status" type="radio" value=""></input>
+            <label for="inStock">In stock</label>
+            <input id="outOfStock" name="status" type="radio" value=""></input>
+            <label for="outOfStock">Out of stock</label>
+        </div>
+      </form>
+    </>
+  );
+}
+
+className = "title-input";
+placeholder = "Add a title to your video";
+name = "title";
+value = { title };
+onChange = { handleTitle };
