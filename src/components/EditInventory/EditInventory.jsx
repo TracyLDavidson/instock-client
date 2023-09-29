70e080c75bcee839b528292d7da7f825aedfce0a
Patrick Harvey <patharvey@protonmail.com>
2023-09-29 12:45:34 -0400

    stage for commit

--- src/components/EditInventory/EditInventory.jsx
+++ src/components/EditInventory/EditInventory.jsx
@@ -1,6 +1,6 @@
 import "./EditInventory.scss";
 import { useState, useEffect } from 'react';
-// import axios from axios;
+import axios from 'axios';
 
 export default function EditInventory() {
 
@@ -9,10 +9,63 @@ export default function EditInventory() {
   const [status, setStatus] = useState('');
   const [quantity, setQuantity] = useState('');
 
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/categories')
+    .then((response) => {
+      setCategories(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }, []);
+
+  const handleCategoryChange = (e) => {
+    setSelectedItem(e.target.value)
+  }
+
   // fetch data from database
-  // useEffect(() => {
-  //   axios.get('http://localhost:8080/inventory/1')
-  // })
+  useEffect(() => {
+    axios.get('http://localhost:8080/inventory/1')
+    .then((response) => {
+      console.log(response.data[0]);
+      const data = response.data[0];
+
+      setItemName(data.item_name);
+      setItemDescription(data.description);
+      setSelectedCategory(data.category); // add category here
+      
+      if (data.status === 'In Stock') {
+        setStatus('inStock');
+      } else if (data.status === 'Out of Stock') { // CHECK SPELLING
+        setStatus('outOfStock');
+      }
+
+      setQuantity(data.quantity);
+      
+    }).catch((error) => {
+      console.error('Error from EditInventory:', error);
+    });
+  }, []);
+
+  // Event Handlers
+  const handleItemNameChange = (e) => {
+    setItemName(e.target.value);
+  };
+
+  const handleItemDescriptionChange = (e) => {
+    setItemDescription(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const handleQuantityChange = (e) => {
+    setQuantity(e.target.value);
+  };
 
   return (
     <>
@@ -27,46 +80,71 @@ export default function EditInventory() {
           <input
             placeholder="Item Name"
             name="item-name"
-            // value={itemName}
-            // onChange={handleItemName}
+            value={itemName}
+            onChange={handleItemNameChange}
             className="input-item"
           ></input>
           <label className="label">Description</label>
           <input
             placeholder="Please enter a brief item description..."
             name="item-description"
-            // value={itemDesc}
-            // onChange={handleItemDesc}
+            value={itemDescription}
+            onChange={handleItemDescriptionChange}
             className="input-description"
           ></input>
-          <label className="label" for="categorySelect">
+          <label className="label" htmlFor="categorySelect">
             Category
           </label>
           <select
             id="categorySelect"
             name="categorySelect"
-            placeholder="Please select"
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            // placeholder="Please select"
             className="select-category"
-          ></select>
+          >
+            <option value="">Please select</option>
+            {categories.map((category) => (
+              <option key={category.category} value={category.category}>
+                {category.category}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="availability">
           <h2 className="availability__title">Item Availability</h2>
           <h3 className="label">Status</h3>
           <div className="availability__status">
-            <input id="inStock" name="status" type="radio" value=""></input>
-            <label for="inStock">In stock</label>
-            <input id="outOfStock" name="status" type="radio" value=""></input>
-            <label for="outOfStock">Out of stock</label>
+            <input 
+            id="inStock" 
+            name="status" 
+            type="radio" 
+            value="inStock"
+            checked={status === 'inStock'}
+            onChange={handleStatusChange}
+            ></input>
+            <label htmlFor="inStock">In stock</label>
+            <input 
+            id="outOfStock" 
+            name="status" 
+            type="radio" 
+            value="outOfStock"
+            checked={status === 'outOfStock'}
+            onChange={handleStatusChange}
+            ></input>
+            <label htmlFor="outOfStock">Out of stock</label>
           </div>
-          <label for="quantityInput" className="label label--quantity">
+          <label htmlFor="quantityInput" className="label label--quantity">
             Quantity
           </label>
           <input
             id="quantityInput"
             name="quantityInput"
             className="input-quantity"
+            value={quantity}
+            onChange={handleQuantityChange}
           ></input>
-          <label for="warehouseSelect" className="label">
+          <label htmlFor="warehouseSelect" className="label">
             Warehouse
           </label>
           <select
