221c3daf266f2b11307857574bfc5381252f299e
tracy_davidson <tracydavidson1@gmail.com>
2023-10-01 02:18:42 -0700

    feature/TITAN-20: Added inventory page component and api function for getting inventory list

--- src/App.jsx
+++ src/App.jsx
@@ -11,64 +11,27 @@ import EditInventory from "./components/EditInventory/EditInventory";
 import WarehouseEditAddPage from "./pages/WarehousesEditAddPage/WarehousesEditAddPage";
 
 function App() {
-  const [inventoryList, setInventoryList] = useState([]); //temporary solution in order useParams with edit
-  const query = "inventory/";
-  const id = "";
-  const url = `http://localhost:8080/${query}${id}`;
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((response) => {
-        setInventoryList(response.data);
-      })
-      .catch((error) => {
-        console.log("error:", error);
-      });
-  }, []);
-  // Commented out because it breaks my code
-  // if (!inventoryList.length) {
-  //   return <>Loading...</>;
-  // }
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        {/* <Route path="/" element={<WarehousesPage />} /> */}
         <Route path="/" element={<Navigate to={`/warehouses`} />} />
         <Route path="/warehouses" element={<WarehousesPage />} />
         <Route
           path="/warehouses/:warehouseId"
           element={<WarehouseInventoryPage />}
         />
-        {/* <Route
-          path="/warehouse/:id/edit"
-          element={<WarehousesPage warehouse={warehouse} />}
-        /> */}
-        {/* <Route
-          path="/warehouse/:id"
-          element={<WarehousesPage warehouse={warehouse} />}
-        /> */}
         <Route
           path="/warehouses/:warehouseID/edit"
           element={<WarehouseEditAddPage />}
         />
         <Route path="/warehouses/add" element={<WarehouseEditAddPage />} />
-        {/* should we leave the following in? discuss with group */}
-        {/* <Route path="/inventory" element={<InventoryPage />} /> */}
-        <Route
-          path="/inventory"
-          element={<Navigate to={`/inventory/${inventoryList[0]?.id}/edit`} />}
-        />
-        {/* <Route path="/inventory/:id" element={<InventoryPage item={item} />} /> */}
+        <Route path="/inventory" element={<InventoryPage />} />
         <Route path="inventory/:id/edit" element={<EditInventory />} />
         <Route
           path="inventory/:id/view"
           element={() => <p>Inventory View Item</p>}
         />
-        {/* should we leave the following in? discuss with group */}
-        {/* <Route path="inventory/add" element={<InventoryPage item={item} />} /> */}
-        {/* <Route path="inventory/add" element={<EditInventory />} /> */}
       </Routes>
       {/* <Footer /> */}
     </BrowserRouter>
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,3 +1,107 @@
-export default function Inventory() {
-  return <></>;
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetchInventory } from "../../utils/api";
+import { Table } from "../../components/Table/Table";
+import { PageHeader } from "../../components/PageHeader/PageHeader";
+import { Paper } from "../../components/Paper/Paper";
+import { PageActions } from "../../components/PageActions/PageActions";
+import { PrimaryButton } from "../../components/PrimaryButton/PrimaryButton";
+import editIconWhite from "../../assets/icons/edit-white.svg";
+
+import "./InventoryPage.scss";
+
+const allowableProperties = ["item_name", "category", "status", "quantity"];
+
+const formatResponse = (data = []) =>
+  data.map((row) => {
+    const filteredRow = {};
+    const keys = Object.keys(row)
+      .filter((key) => allowableProperties.includes(key))
+      .forEach((key) => {
+        filteredRow[key] = row[key];
+      });
+
+    return filteredRow;
+  });
+
+const generateTableHeaderLabels = (rows) => {
+  const headers = Object.keys(rows[0]);
+  const sortable = [true, true, true, true, false];
+  const labels = ["inventory item", "category", "status", "qty"];
+  const setStatusClassName = (tableCellValue) => {
+    let className = "";
+    if (tableCellValue === "In Stock") {
+      className = "chip chip--green";
+    }
+    if (tableCellValue === "Out of Stock") {
+      className = "chip chip--red";
+    }
+    return className;
+  };
+
+  const tableHeaders = headers.map((header, index) => {
+    return {
+      key: header,
+      label: labels[index],
+      sortable: sortable[index],
+      className: setStatusClassName,
+    };
+  });
+
+  tableHeaders.push({
+    key: "actions",
+    label: "actions",
+    sortable: false,
+    className: () => {},
+  });
+  return tableHeaders;
+};
+
+export default function WarehouseInventoryPage() {
+  const navigate = useNavigate();
+  const [sortBy, setSortBy] = useState("");
+  const [tableHeaders, setTableHeaders] = useState([]);
+  const [tableRows, setTableRows] = useState([]);
+  const [sourceRows, setSourceRows] = useState([]);
+
+  useEffect(() => {
+    const effects = async () => {
+      try {
+        const inventoryRes = await fetchInventory();
+        if (inventoryRes.data) {
+          const rows = formatResponse(inventoryRes.data);
+          const headers = generateTableHeaderLabels(rows);
+          setTableHeaders(headers);
+          setTableRows(rows);
+          setSourceRows(inventoryRes.data);
+        }
+      } catch (e) {}
+    };
+    effects();
+  }, []);
+
+  return (
+    <div className="inventory_page">
+      <div className="inventory_page__container">
+        <Paper>
+          <PageHeader title={"Inventory"} onNavigateBack={() => navigate("/")}>
+            <PrimaryButton>
+              <img src={editIconWhite} alt="An edit icon" />
+              Edit
+            </PrimaryButton>
+          </PageHeader>
+
+          <Table
+            headers={tableHeaders}
+            rows={tableRows}
+            sourceRows={sourceRows}
+            actionsComponent={() => <PageActions />}
+            onTableSort={setSortBy}
+            sortBy={sortBy}
+            onRowClick={(row) => navigate(`/inventory/${row.id}/view`)}
+          />
+        </Paper>
+      </div>
+    </div>
+  );
 }
